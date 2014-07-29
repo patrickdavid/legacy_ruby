b00060692442e5e2d5c6085ead27d9e4f2ec0e60
Patrick David & Brandon Madsen <student@epicodus.com>
2014-07-29 11:11:00 -0700

    passes the entire hash into the modified hash

--- lib/legacy.rb
+++ lib/legacy.rb
@@ -2,12 +2,8 @@ def legacy(hash)
     new_hash = Hash.new
     hash.each do |key,value|
       value.each do |element|
-        puts new_hash
-        puts new_hash[element] = key
+        new_hash[element] = key
       end
     end
-    puts new_hash
     new_hash
   end
-
-legacy({1=>['A', 'E', 'I']})
