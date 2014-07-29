1a8579ed2942cfa286009dc1615b5b1695fce107
Patrick David & Brandon Madsen <student@epicodus.com>
2014-07-29 10:58:11 -0700

    pass multiple elements with the value of 1 into an empty array

--- lib/legacy.rb
+++ lib/legacy.rb
@@ -1,8 +1,13 @@
 def legacy(hash)
-    new_hash = {}
+    new_hash = Hash.new
     hash.each do |key,value|
-      new_hash = {value[0] => key}
+      value.each do |element|
+        puts new_hash
+        puts new_hash[element] = key
+      end
     end
     puts new_hash
     new_hash
   end
+
+legacy({1=>['A', 'E', 'I']})
--- spec/legacy_spec.rb
+++ spec/legacy_spec.rb
@@ -1,8 +1,11 @@
-require "legacy"
-require "rspec"
+require ("legacy")
+require ("rspec")
 
 describe("legacy") do
   it("takes the character 'A' and gives it the value of one") do
     expect(legacy({1=>['A']})).to(eq({'A'=>1}))
+  end
+  it("takes the character 'A', 'E', 'I' and gives it the value of one") do
+    expect(legacy({1=>['A', 'E', 'I']})).to(eq({'A'=>1, 'E'=>1, 'I'=>1}))
   end
 end
